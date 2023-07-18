229edc7563193fc5687606f518ad58ed295fe3de
Timur N <tnuerekenov200@gmail.com>
2023-07-18 17:50:06 +0600

    calc

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { getMatIconFailedToSanitizeUrlError } from '@angular/material/icon';
 
-export interface Btn {
-  text:string,
-  color:string;
+export interface Tile {
+  cols:number;
+  rows:number;
+  text?:string,
+  type:string;
+  click?:any;
+  color?:string;
 }
 
 @Component({
@@ -11,22 +16,85 @@ export interface Btn {
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+  total: any = 0;
+  operator = null;
+  waitForSecondNumber = false;
+  firstOperand: any = null;
   inputValue!: string;
-  buttons: Btn[] = [
-    { text: '1', color: 'primary' },
-    { text: '2', color: 'primary' },
-    { text: '3', color: 'primary' },
-    { text: '4', color: 'primary' },
-    { text: '5', color: 'primary' },
-    { text: '6', color: 'primary' },
-    { text: '7', color: 'primary' },
-    { text: '8', color: 'primary' },
-    { text: '9', color: 'primary' },
-    { text: '0', color: 'primary' },
-    { text: '+', color: 'primary' },
-    { text: '-', color: 'primary' },
-    { text: '*', color: 'primary' },
-    { text: '/', color: 'primary' },
-    { text: '=', color: 'primary' }
+  buttons: Tile[] = [
+    {cols: 4, rows: 1, type:'field'},
+    { text: 'AC',cols: 1, rows:1, type: 'button', click: 'clear', color: 'primary' },
+    { text: '+',cols: 1, rows:1, type: 'button', click: 'operator', color: 'primary' },
+    { text: '%',cols: 1, rows:1, type: 'button', click: 'clear', color: 'primary' },
+    { text: '-',cols: 1, rows:1, type: 'button', click: 'operator', color: 'primary' },
+    
+    { text: '7',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '8',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '9',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '*',cols: 1, rows:1, type: 'button', click: 'operator', color: 'primary' },
+    
+    { text: '6',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '5',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '4',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '/',cols: 1, rows:1, type: 'button', click: 'operator', color: 'primary' },
+    
+    { text: '3',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '2',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '1',cols: 1, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '=',cols: 1, rows:1, type: 'button', click: 'operator', color: 'primary' },
+
+    { text: '0',cols: 2, rows:1, type: 'button', click: 'number', color: 'primary' },
+    { text: '<-',cols: 2, rows:1, type: 'button', click: 'operator', color: 'primary' },
+    { text: '.',cols: 1, rows:1, type: 'button', click: 'decimal', color: 'primary' },
   ];
+  eventClick(event: string, param:any = null){
+    switch(event) {
+      case "number":
+        if (this.waitForSecondNumber) {
+          this.total = param
+          this.waitForSecondNumber = false;
+        }
+        else {
+          this.total = (this.total === 0) ? Number(param) : Number('' + this.total+param)
+        }
+        break;
+      case "operator":
+        this.operator = param;
+        if (this.firstOperand === null) {
+          this.firstOperand = Number(this.total)
+        }
+        else if(this.operator) {
+          const result = this.doCalculation(this.operator, Number(this.total))
+          this.total = String(result);
+          this.firstOperand = result
+        }
+        this.waitForSecondNumber = true;
+        break;
+      case "clear":
+        this.total = 0;
+        this.firstOperand = null
+        this.operator = null
+        this.waitForSecondNumber = false
+        break;
+      case "decimal":
+        if (!this.total.includes('.')) this.total += '.';
+        break;
+    }
+  }
+
+  private doCalculation(operator: string,secondNumber: any) {
+    switch(operator) {
+      case '+':
+        return this.firstOperand +=secondNumber
+      case '-':
+        return this.firstOperand -=secondNumber
+      case '*':
+        return this.firstOperand *=secondNumber
+      case '/':
+        return this.firstOperand /=secondNumber  
+      case '=':
+        return secondNumber
+    }
+  }
+
 }
